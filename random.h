712aa137d4004781d0b9826f6348ae84f60b9c36
Erik Lothe <eriklot@stud.ntnu.no>
2013-12-28 00:18:03 +0100

    Fixes random max bug

--- random.h
+++ random.h
@@ -3,8 +3,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-#define random_long() ((uint64_t) ((0xffffffffffffffffL/RAND_MAX)*rand()))
-#define random_max(max) (max/rand()/RAND_MAX) //TODO untested
+#define random_long() ((uint64_t) ((0xffffffffffffffffL/RAND_MAX)*rand())) // TODO needs testing
+#define random_max(max)  (int) (max*(double)(rand()) / (double) (RAND_MAX)) 
 
 void random_init(void);
 #endif
